d57ff8c16cc1ea4e8e1b28421da119abacfb9a8f
Antonio <antonio@mochimedia.com>
2013-07-10 16:33:53 -0700

    Success!

--- grader.js
+++ grader.js
@@ -28,7 +28,6 @@ var cheerio = require('cheerio');
 var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = rest.get('http://aqueous-journey-7597.herokuapp.com');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -68,7 +67,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url>', 'Path to site', clone(assertFileExists), URL_DEFAULT)
+        .option('-u, --url <url>', 'http://aqueous-journey-7597.herokuapp.com')
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
