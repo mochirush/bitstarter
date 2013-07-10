3e34f2774279e4bf65c1a4f1369b5646875ee93e
antonio@mochimedia.com <example@stanford.edu>
2013-07-10 20:32:19 +0000

    Changed express to non deprecated

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 var index_contents = fs.readFileSync('index.html');
 
