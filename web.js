75dfbc16a3693a9932ba874dc8b3a11e5f64d7e6
antonio@mochimedia.com <example@stanford.edu>
2013-07-10 20:11:19 +0000

    Test to see if file being read

--- web.js
+++ web.js
@@ -4,11 +4,5 @@ var app = express.createServer(express.logger());
 
 var index_contents = fs.readFileSync('index.html');
 
-app.get('/', function(request, response) {
-  response.send(index_contents);
-});
+console.log(index_contents);
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
